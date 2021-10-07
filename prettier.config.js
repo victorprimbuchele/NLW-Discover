878d28c8d867323107e9a87b1ce9bcc146b90e64
Victor Prim <victor.buchele@jdrel.com.br>
2021-10-07 17:38:36 -0300

    Projeto NLW-Discover. O presente commit refere-se à estrutura de código realizada até a terceira aula aos 45 minutos.

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    trailingComma: "es5",
+
+    tabWidth: 4, // tamanho do espaço inicial na identacao
+
+    semi: true, // adiciona ;
+
+    singleQuote: true, // substitui "double quote" p 'single quote'
+
+    bracketSpacing: true, // espacamento entre as chaves
+
+    arrowParens: "always", //sempre havera () em functions mesmo in line
+
+};
